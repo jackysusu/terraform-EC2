d2b2234de79d26c6801a0d0298af371e57d5987e
jackysusu <58243487+jackysusu@users.noreply.github.com>
2022-08-28 10:21:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#jacky disgn 1
+#jacky disgn 12
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
